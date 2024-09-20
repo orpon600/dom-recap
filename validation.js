6d0d4353d1b81d30ba31f77cc7cf0f70bf4f4ea4
orpon600 <orponmahanta775@gmail.com>
2024-09-21 04:15:30 +0600

    first commit

---
+++ validation.js
@@ -0,0 +1,13 @@
+const number = "23";
+//string, number, boolean
+if (typeof number === "number") {
+  console.log("value is a number");
+} else {
+  console.log("value is not a number");
+}
+
+const numbers = [3, 4, 5, 67];
+console.log(Array.isArray(numbers));
+
+const student = { name: "Rongila Rafsan", job: "khawadawa kora" };
+console.log(typeof student);
